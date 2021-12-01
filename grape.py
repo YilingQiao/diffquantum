32b6e33a1abe8aea78ff680d384e900194a283e0
YilingQiao <yilingq@umd.edu>
2021-12-01 12:49:20 -0500

    minimize the energy

--- grape.py
+++ grape.py
@@ -19,20 +19,26 @@ def random_initialize_u(n_step, n_Hs):
     return u
 
 
-class Grape():
+class Grape(object):
+    """A class for pulse-based simulation and optimization
+    Args:
+        taylor_terms: number of taylor expansion terms.
     """
-    TODO: 
-        measure
-
-    """
-    def __init__(self, 
-        taylor_terms=5):
-
-        # self.init_u = init_u
-        # self.Hs = Hs
+    def __init__(self, taylor_terms=5):
         self.taylor_terms = taylor_terms
 
-    def train(self, init_u, H0, Hs, initial_states, target_states, dt):
+    def train_fidelity(self, init_u, H0, Hs, initial_states, target_states, dt):
+        """Control the systems to reach target states.
+        Args:
+            init_u: initial pulse.
+            H0: a Hermitian matrix.
+            Hs: a list of Hermitian matrics.
+            initial_states: initial_states.
+            target_states: target_states.
+            dt: size of time step.
+        Returns:
+            us: optimized pulses.
+        """
         lr = 3e-3
         n_epoch = 200
         w_l2 = 1
@@ -45,17 +51,10 @@ class Grape():
         target_states = torch.tensor(target_states).double().transpose(1, 0)
 
         for epoch in range(n_epoch):
-
             modifled_us = torch.sin(us) * max_amplitude
             final_states = self.forward_simulate(modifled_us, H0, Hs, initial_states)
-            # print("final_states", final_states)
-            # print("target_states", target_states)
-            # exit()
             loss_fidelity = 1 - self.get_inner_product_2D(final_states, target_states)
             loss_l2 = torch.sqrt((us**2).mean())
-
-            # print(final_states, target_states)
-
             loss = loss_fidelity + loss_l2 * w_l2
             optimizer.zero_grad()
             loss.backward()
@@ -65,15 +64,52 @@ class Grape():
                 float(loss.detach().numpy()), 
                 float(loss_fidelity.detach().numpy()))
             )
+        return us
 
+    def train_energy(self, M, init_u, H0, Hs, psi0, dt):
+        """Optimize the pulse to minimize the energy <psi(1)|M|psi(1)>
+        Args:
+            M: a Hermitian matrix.
+            init_u: initial pulse.
+            H0: a Hermitian matrix.
+            Hs: a list of Hermitian matrics.
+            psi0: initial state.
+            dt: size of time step.
+        Returns:
+            us: optimized pulses.
+        """
+        lr = 2e-2
+        n_epoch = 200
+        w_l2 = 0
+        Hs = [torch.tensor(c_to_r_mat(-1j * dt * H)) for H in Hs]
+        H0 = torch.tensor(c_to_r_mat(-1j * dt * H0)) 
+        us = torch.tensor(init_u, requires_grad=True)
+        optimizer = torch.optim.Adam([us], lr=lr)
+        initial_states = torch.tensor(psi0).double().unsqueeze(-1)
+        M = torch.from_numpy(M)
+
+        for epoch in range(n_epoch):
+            v = self.forward_simulate(us, H0, Hs, initial_states)
+            loss_energy = v.transpose(1, 0).matmul(M).matmul(v)
+            loss_l2 = torch.sqrt((us**2).mean())
+            loss = loss_energy + loss_l2 * w_l2
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print("epoch: {:04d}, loss: {:.4f}, loss_energy: {:.4f}".format(
+                epoch, 
+                float(loss.detach().numpy()), 
+                float(loss_energy.detach().numpy()))
+            )
         return us
 
     def forward_simulate(self, us, H0, Hs, initial_states):
         """Forward time evolution.
         Args:
+            us: pulses.
+            H0: a Hermitian matrix.
+            Hs: a list of Hermitian matrics.
             initial_states: initial states.
-            us: n_Time x n_Hs - amplitudes.
-            Hs: n_Hs - hamiltonians.
         Returns:
             final_states: final states.
         """
@@ -92,8 +128,13 @@ class Grape():
         return final_states
 
     def get_inner_product_2D(self,psi1,psi2):
-        #Take 2 states psi1,psi2, calculate their overlap, for arbitrary number of vectors
-        # psi1 and psi2 are shaped as (2*state_num, number of vectors)
+        """compute the inner product |psi1.dag() * psi2|.
+        Args:
+            psi1: a state.
+            psi2: a psi2.
+        Returns:
+            norm: norm of the complex number.
+        """
         state_num = int(psi1.shape[0] / 2)
         
         psi_1_real = psi1[0:state_num,:]
@@ -110,32 +151,9 @@ class Grape():
         norm = (reals + imags) / (psi1.shape[1]**2)
         return norm
 
-    def inner_product(self, p0, p1):
-        """Inner product of p0 p1.
-        p0 = a + ib
-        p1 = c + id
-        Args:
-            p0:
-            p1:
-        Returns:
-            ans: <p0|p1>
-        """
-        p0 = torch.reshape(p0, [2, -1])
-        p1 = torch.reshape(p1, [2, -1])
-        a, b ,c, d = p0[0], p0[1], p1[0], p1[1]
-        ac = (a * c).sum()
-        bd = (b * d).sum()
-        bc = (b * c).sum()
-        ad = (a * d).sum()
-        reals = (ac + bd)**2
-        imags = (bc - ad)**2
-        norm = (reals + imags) / (p0.shape[1]**2)
-
-        return norm
-
 
     def compute_op(self, us, Hs):
-        """Compute the time operator in a short time
+        """Compute the time operator in a short time using taylor expansion.
         Args:
             us: n_Hs - amplitudes.
             Hs: n_Hs - hamiltonians.
@@ -158,33 +176,63 @@ class Grape():
             op += Hn / factorial
         return op
 
+    def demo_fidelity(self):
+        """demo of control the psi(1) to be the target states.
+        """
+        qubit_state_num = 2
+        qubit_num = 1
+        freq_ge = 3.9 # GHz
+        dt = 0.01
+        n_step = 100
+
+        Q_x = np.diag(np.sqrt(np.arange(1, qubit_state_num)), 1) \
+                + np.diag(np.sqrt(np.arange(1, qubit_state_num)), -1)
+        Q_y = (0+1j) *(np.diag(np.sqrt(np.arange(1, qubit_state_num)), 1) \
+                - np.diag(np.sqrt(np.arange(1, qubit_state_num)), -1))
+        Q_z = np.diag(np.arange(0, qubit_state_num))
+        ens = np.array([2 * np.pi * ii * freq_ge for ii in np.arange(qubit_state_num)])
+        H_q = np.diag(ens)
+        H0 = H_q
+        H0 = np.eye(qubit_state_num)
+        Hs = [Q_x]
+
+        g = np.array([1,0])
+        e = np.array([0,1])
+        psi0 = [g, e]
+            
+        psi1 = [e, g]
+        target_states = [c_to_r_vec(v) for v in psi1]
+        initial_states = [c_to_r_vec(v) for v in psi0]
+
+        init_u = random_initialize_u(n_step, len(Hs))
+        final_u = self.train_fidelity(init_u, H0, Hs, initial_states, target_states, dt)
+
+    def demo_energy(self):
+        """demo of optimizing an energy <psi(1)|M|psi(1)>.
+        """
+        qubit_state_num = 2
+        qubit_num = 1
+        dt = 0.01
+        n_step = 100
+        M = np.array([[1, 3 + 1.j], [3 - 1.j, 2]])
+        M = c_to_r_mat(M)
+
+        Q_x = np.diag(np.sqrt(np.arange(1, qubit_state_num)), 1) \
+                + np.diag(np.sqrt(np.arange(1, qubit_state_num)), -1)
+        Q_y = (0+1j) *(np.diag(np.sqrt(np.arange(1, qubit_state_num)), 1) \
+                - np.diag(np.sqrt(np.arange(1, qubit_state_num)), -1))
+        Q_z = np.diag(np.arange(0, qubit_state_num))
+        H0 = np.eye(qubit_state_num)
+        Hs = [Q_x]
+
+        g = np.array([1,0])
+        e = np.array([0,1])
+        psi0 = c_to_r_vec(g)
+
+        init_u = random_initialize_u(n_step, len(Hs))
+        final_u = self.train_energy(M, init_u, H0, Hs, psi0, dt)
+
 if __name__ == '__main__':
-    qubit_state_num = 2
-    qubit_num = 1
-    freq_ge = 3.9 # GHz
-    dt = 0.01
-    n_step = 100
-
-    Q_x = np.diag(np.sqrt(np.arange(1, qubit_state_num)), 1) \
-            + np.diag(np.sqrt(np.arange(1, qubit_state_num)), -1)
-    Q_y = (0+1j) *(np.diag(np.sqrt(np.arange(1, qubit_state_num)), 1) \
-            - np.diag(np.sqrt(np.arange(1, qubit_state_num)), -1))
-    Q_z = np.diag(np.arange(0, qubit_state_num))
-    ens = np.array([2 * np.pi * ii * freq_ge for ii in np.arange(qubit_state_num)])
-    H_q = np.diag(ens)
-    H0 = H_q
-    H0 = np.eye(qubit_state_num)
-    Hs = [Q_x]
-
-    g = np.array([1,0])
-    e = np.array([0,1])
-    psi0=[g, e]
-        
-    psi1 = [e, g]
-    target_states = [c_to_r_vec(v) for v in psi1]
-    initial_states = [c_to_r_vec(v) for v in psi0]
-
-    init_u = random_initialize_u(n_step, len(Hs))
-
     grape = Grape(taylor_terms=20)
-    final_u = grape.train(init_u, H0, Hs, initial_states, target_states, dt)
+    grape.demo_fidelity()
+    # grape.demo_energy()
--- ours_spectral.py
+++ ours_spectral.py
@@ -3,15 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from grape import *
 
-class OurFourier(object):
-    """Use Fourier series to represent the amplitude.
+class OurSpectral(object):
+    """A class for using Fourier series to represent the amplitudes.
     The derivatives are computed by our method.
+    Args:
+        n_basis: number of Fourier basis.
     """
     def __init__(self, n_basis=20):
         self.n_basis = n_basis
         return
 
     def generate_u(self, i):
+        """Generate the function u(i) for H_i
+        Args:
+            i: index of the H_i.
+        Returns:
+            _u: function u_i(t).
+        """
         def _u(t, args):
             coeff_i = self.spectral_coeff.detach().numpy()[i]
             u = 0
@@ -20,12 +28,19 @@ class OurFourier(object):
             return u
         return _u
 
-
-    def compute_grad_MC(self, H, initial_states, target_states, M):
+    def compute_energy_grad_MC(self, M, H, initial_state):
+        """Compute the gradient of engergy function <psi(1)|M|psi(1)>
+        Args:
+            M: A Hermitian matrix.
+            H: Hamiltonians [H0, [H_i, u_i(t)], ...].
+            initial_state: initial_state.
+        Returns:
+            grad_coeff: gradients of the spectral coefficients.
+        """
         grad_coeff = np.zeros(self.spectral_coeff.shape)
         s = np.random.uniform()
         t0s = np.linspace(0, s, self.n_step)
-        result = mesolve(H, initial_states, t0s)
+        result = mesolve(H, initial_state, t0s)
         phi = result.states[-1]
         
         ts1 = np.linspace(s, 1, self.n_step)
@@ -48,58 +63,62 @@ class OurFourier(object):
             for j in range(self.n_basis):
                 grad_coeff[i][j] = np.cos(2 * np.pi * j * s) * ps
 
-        self.spectral_coeff.grad = torch.from_numpy(grad_coeff)
+        return torch.from_numpy(grad_coeff)
 
-
-    def train(self, H0, Hs, initial_states, target_states, n_step):
+    def train_energy(self, M, H0, Hs, initial_state, n_step):
+        """Train the sepctral coefficients to minimize energy minimization.
+        Args:
+            M: A Hermitian matrix.
+            H0: The drift Hamiltonian.
+            Hs: Controlable Hamiltonians.
+            initial_state: initial_state.
+            n_step: number of time steps.
+        Returns:
+            spectral_coeff: sepctral coefficients.
+        """
         self.n_step = n_step
         self.n_Hs = len(Hs)
         coeff = np.random.normal(0, 1, [self.n_Hs ,self.n_basis]) 
         self.spectral_coeff = torch.tensor(coeff, requires_grad=True)
 
-        lr = 8e-3
+        lr = 2e-2
         n_epoch = 200
         w_l2 = 1
-
-        M = np.array([[1, 1 + 1.j], [1 - 1.j, 2]])
-        M = Qobj(M)
-
         I = qeye(2)
         ts = np.linspace(0, 1, n_step) 
 
         optimizer = torch.optim.Adam([self.spectral_coeff], lr=lr)
-
         for epoch in range(n_epoch):
-
             H = [H0]
             for i in range(self.n_Hs):
                 H.append([Hs[i], self.generate_u(i)])
-            
-            result = mesolve(H, initial_states, ts)
+            result = mesolve(H, initial_state, ts)
             final_state = result.states[-1]
 
             loss_energy = M.matrix_element(final_state, final_state)
             loss = loss_energy
-
             print("epoch: {:04d}, loss: {:.4f}, loss_energy: {:.4f}".format(
                 epoch, 
                 loss.real, 
                 loss_energy.real
             ))
-
             optimizer.zero_grad()
-            self.compute_grad_MC(H, initial_states, target_states, M)
+            grad_coeff = self.compute_energy_grad_MC(M, H, initial_state)
+            self.spectral_coeff.grad = grad_coeff
             optimizer.step()
+        return self.spectral_coeff
 
+    def demo_energy(self):
+        Hs = [sigmax()]
+        H0 = qeye(2)
+        g, e = basis(2, 0), basis(2, 1)
+        psi0 = g
+        n_step = 100
+        M = np.array([[1, 3 + 1.j], [3 - 1.j, 2]])
+        M = Qobj(M)
+        self.train_energy(M, H0, Hs, psi0, n_step)
 
 if __name__ == '__main__':
-    Hs = [sigmax()]
-    H0 = qeye(2)
-    g, e = basis(2, 0), basis(2, 1)
-    psi0 = g
-    psi1 = e
-    n_step = 100
-
-    ours = OurFourier()
-    ours.train(H0, Hs, psi0, psi1, n_step)
+    ours_spectral = OurSpectral()
+    ours_spectral.demo_energy()
 
