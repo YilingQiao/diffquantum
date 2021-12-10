5e9c9d874dca47fbac9ae3330d085a6d61b2ed99
YilingQiao <yilingq@umd.edu>
2021-12-10 00:01:29 -0500

    debug ket

--- plot_finite_diff.py
+++ plot_finite_diff.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ours_spectral import OurSpectral
 
-# ours_spectral = OurSpectral(basis='Fourier', n_basis=2)
-ours_spectral = OurSpectral(basis='Legendre', n_basis=2)
+# ours_spectral = OurSpectral(basis='Fourier', n_basis=6)
+ours_spectral = OurSpectral(basis='Legendre', n_basis=3)
 grad_ours, grad_finite_diff = ours_spectral.demo_finite_diff(n_samples=3000, delta=1e-5)
 print("basis: ", ours_spectral.basis)
 print("finite difference: ")
