b9ee5466a47a463c589464ecfd50cccf7b5921ad
YilingQiao <yilingq@umd.edu>
2021-12-26 21:35:19 -0500

    optimizer

--- OursPulse.py
+++ OursPulse.py
@@ -11,15 +11,17 @@ from qiskit.providers.aer import QasmSimulator, PulseSimulator
 from qiskit.providers.aer.pulse import PulseSystemModel
 from qiskit.tools.monitor import job_monitor
 
+import warnings
+warnings.filterwarnings('ignore')
 
 
 def normC(x, y) :
-    l = sqrt(x ** 2 + y ** 2)
+    l = np.sqrt(x ** 2 + y ** 2)
     coef = (2 * expit(l) - 1) / l
     return coef * x, coef * y
 
 def dnormC(x, y) :
-    l = sqrt(x ** 2 + y ** 2)
+    l = np.sqrt(x ** 2 + y ** 2)
     sigl = expit(l)
     sigl21_l = (2 * sigl - 1) / l
     d_sigl21_l = 2 * sigl * (1 - sigl) - sigl21_l
@@ -63,7 +65,7 @@ class OursPulse(object):
         n_basis: number of Fourier basis.
     """
     def __init__(self, n_basis=5, basis='Fourier', n_epoch=200, n_step=100, 
-        lr=2e-2, is_noisy=False, T=100, n_shots=8192):
+        lr=2e-2, is_noisy=False, T=128, n_shots=8192, n_qubit=1):
 
         self.n_basis = n_basis
         self.log_dir = "./logs/"
@@ -75,13 +77,19 @@ class OursPulse(object):
         self.is_noisy = is_noisy
         self.T = T
         self.n_shots = n_shots
+        self.n_qubit = n_qubit
         if basis == 'Legendre':
             self.legendre_ps = [legendre(j) for j in range(self.n_basis)]
+        self.start_engine()
 
     def start_engine(self, hub='ibm-q-community', group='ibmquantumawards', project='open-science-22'):
+        
+        if IBMQ.active_account() is None:
+            IBMQ.load_account()
+
         print("provider: ", hub, group, project)
-        self.provider = IBMQ.get_provider(hub=hub, group=hub, project=project)
-        self.backend = provider.get_backend('ibmq_jakarta')
+        self.provider = IBMQ.get_provider(hub=hub, group=group, project=project)
+        self.backend = self.provider.get_backend('ibmq_jakarta')
         #sim_noisy_jakarta = QasmSimulator.from_backend(backend)
         self.sim_backend = PulseSimulator()
         self.backend_model = PulseSystemModel.from_backend(self.backend)
@@ -90,62 +98,63 @@ class OursPulse(object):
     @staticmethod
     def experiemnt(vRI, T, n_shots, backend, sim_backend, 
         s=0, qbt=-1, theta=0, phi=0, lam=0):
-        n_qubit = vRl.shape[0] 
+        n_qubit, n_basis = vRI.shape[0], vRI.shape[1]  
+
         with pulse.build(backend) as pulse_prog :
             for i in range(n_qubit) :
                 channel = pulse.drive_channel(i)
                 seq = [mapC(*value(n_basis, vRI[i,:,0], vRI[i,:,1], T, t)) for t in range(0, s)]
                 if seq != [] :
                     pulse.play(seq, channel)
-                if k != -1 :
-                    pulse.u3(theta, phi, lam, k) # apply u3(theta, phi, lam) on qubit k
+                if qbt != -1 :
+                    pulse.u3(theta, phi, lam, qbt) # apply u3(theta, phi, lam) on qubit k
                 seq = [mapC(*value(n_basis, vRI[i,:,0], vRI[i,:,1], T, t)) for t in range(s, T)]
                 if seq != [] :
                     pulse.play(seq, channel)
                 pulse.barrier(i)
                 reg = pulse.measure(i)
-        job = execute(pulse_prog, sim_backend, shots = shots)
+        job = execute(pulse_prog, sim_backend, shots=n_shots)
         res = job.result()
 
-        # global sch
-        # sch = pulse_prog
-        return calcM(res.get_counts(), shots)
+        global sch
+        sch = pulse_prog
+        return calcM(res.get_counts(), n_shots)
 
 
     def grad_energy_MC(self):
         grad_vRI = np.zeros(self.spectral_coeff.shape)
         vRI = self.spectral_coeff.detach().numpy()
 
-        s = np.random.randint(T)
+        s = np.random.randint(self.T)
 
         for qbt in range(self.n_qubit) :
             pm = self.experiemnt(vRI, self.T, self.n_shots, self.backend, self.sim_backend, 
                 s=s, qbt=qbt, theta=np.pi / 2, phi=-np.pi / 2, lam=np.pi / 2)
             pp = self.experiemnt(vRI, self.T, self.n_shots, self.backend, self.sim_backend, 
                 s=s, qbt=qbt, theta=-np.pi / 2, phi=-np.pi / 2, lam=np.pi / 2)
-            dL_dR = 1. / sqrt(2) * (pm - pp)
+            dL_dR = 1. / np.sqrt(2) * (pm - pp)
 
 
             pm = self.experiemnt(vRI, self.T, self.n_shots, self.backend, self.sim_backend, 
                 s=s, qbt=qbt, theta=np.pi / 2, phi=0, lam=0)
             pp = self.experiemnt(vRI, self.T, self.n_shots, self.backend, self.sim_backend, 
                 s=s, qbt=qbt, theta=-np.pi / 2, phi=0, lam=0)
-            dL_dR = 1. / sqrt(2) * (pm - pp)
-            dL_dI = 1. / sqrt(2) * (pm - pp)
+            dL_dI = 1. / np.sqrt(2) * (pm - pp)
 
             grad_vRI[qbt,:,0], grad_vRI[qbt,:,1] = dvalue(
-                self.n_basis, vRI[qbt,:,0], vRI[qbt,:,1], T, s, dL_dR, dL_dI)
+                self.n_basis, vRI[qbt,:,0], vRI[qbt,:,1], self.T, s, dL_dR, dL_dI)
         return grad_vRI
 
 
-    def train_energy():
-
-        self.n_Hs = len(Hs)
+    def train_energy(self):
         coeff = np.random.normal(0, 1e-3, [self.n_qubit ,self.n_basis, 2]) 
         # coeff = np.ones([self.n_qubit ,self.n_basis, 2])
         self.spectral_coeff = torch.tensor(coeff, requires_grad=True)
         qr = QuantumRegister(self.n_qubit)
         losses = []
+
+        optimizer = torch.optim.Adam([self.spectral_coeff], lr=self.lr)
+
         for epoch in range(self.n_epoch):
             vRI = self.spectral_coeff.detach().numpy()
             loss = self.experiemnt(vRI, self.T, self.n_shots, self.backend, self.sim_backend)
@@ -155,7 +164,6 @@ class OursPulse(object):
             self.spectral_coeff.grad += grad_coeff
             optimizer.step()
 
-
             print("epoch: {:04d}, loss: {:.4f}, loss_energy: {:.4f}".format(
                 epoch, 
                 loss, 
@@ -163,13 +171,11 @@ class OursPulse(object):
             ))
 
 
-    def demo_X():
+    def demo_X(self):
+        self.train_energy()
 
-
-        pass
 if __name__ == '__main__':
-    ours_pulse = OursPulse(basis='Legendre', n_basis=6, n_epoch=200)
-    ours_pulse.start_engine()
+    ours_pulse = OursPulse(basis='Legendre', n_basis=4, T=16)
     ours_pulse.demo_X()
     
 
