7c95426e89589cb7c20480909a80828e36f50e5b
YilingQiao <yilingq@umd.edu>
2022-01-12 00:26:10 -0500

    add more channels

--- ibmsim_icml22.py
+++ ibmsim_icml22.py
@@ -15,18 +15,12 @@ class QubitControl(object):
     Args:
         n_basis: number of Fourier basis.
     """
-    def __init__(self, dt=0.22, eps=32.901, Amax=0.955, freq=2*np.pi*5.236, duration=96,
+    def __init__(self, dt=0.22, duration=96,
                  n_basis=5, basis='Legendre', n_epoch=200, lr=1e-2, 
                  is_sample_discrete=False, is_noisy=False):
         self.dt = dt
-        self.raw_eps = eps
-        self.raw_Amax = Amax
-        self.raw_freq = freq
         self.duration = duration
         
-        self.eps = eps * dt
-        self.Amax = Amax * dt
-        self.w = freq * dt
         
         self.n_basis = n_basis
         self.log_dir = "./logs/"
@@ -103,7 +97,7 @@ class QubitControl(object):
         psi0 = qp.Qobj(psi0)
         return psi0
 
-    def full_pulse(self, vv, i=None):
+    def full_pulse(self, vv, channels):
         """Generate the full driving pulse for H1 = X
         Args:
             vv: parameters for the ansatz
@@ -124,28 +118,35 @@ class QubitControl(object):
         #     return np.cos(self.w * t) * (2 * scipy.special.expit(u) - 1)
 
 
+        # j, omega, freq, idx
 
         def _D(t, args):
             # t ranges from [0, duration]
             A = 0
             B = 0
-            if i is None:
-                coeff_i = vv
-            else:
-                coeff_i = vv[:, i,:]
-            for j in range(self.n_basis):
-                A += coeff_i[0, j] * legendre(j)(2 * t / self.duration - 1)
-                B += coeff_i[1, j] * legendre(j)(2 * t / self.duration - 1)
-            
-            N = np.sqrt(A**2 + B**2)
+            ans = 0
 
-            return self.Amax * (2 * scipy.special.expit(N) - 1)/N * (np.cos(self.w * t) * A + np.sin(self.w * t) * B)
+            for chan in channels:
+                i, omega, w, idx = chan
+                coeff_i = vv[:, idx, :]
+
+                for j in range(self.n_basis):
+                    A += coeff_i[0, j] * legendre(j)(2 * t / self.duration - 1)
+                    B += coeff_i[1, j] * legendre(j)(2 * t / self.duration - 1)
+            
+                N = np.sqrt(A**2 + B**2)
+                ans += omega * (2 * scipy.special.expit(N) - 1)/N * (np.cos(w * t) * A + np.sin(w * t) * B)
+
+            #     print(omega, w)
+            # print(t, ans)
+            # exit()
+            return ans
 
 
         return _D
     
     
-    def get_integrand(self, _H, M, initial_state, s):
+    def get_integrand(self, H0, Hs, M, initial_state, s):
         
         integrand = np.zeros(self.vv.shape)
         
@@ -153,68 +154,64 @@ class QubitControl(object):
         #param = torch.clone(self.vv)
         #param.requires_grad = True
         sgm = torch.nn.Sigmoid()
-        for i in range(1, len(_H)):
 
-            legendre_A = [self.vv[0,i-1,j] * legendre(j)(2 * s / self.duration - 1) for j in range(self.n_basis)]
-            legendre_B = [self.vv[1,i-1,j] * legendre(j)(2 * s / self.duration - 1) for j in range(self.n_basis)]
-            A = sum(legendre_A)
-            B = sum(legendre_B)
-            N = torch.sqrt(torch.square(A)+torch.square(B))
-            Ds = self.Amax * (2 * sgm(N) - 1)/N *(np.cos(self.w * s) * A + np.sin(self.w * s) * B)
-            Ds.backward()
+        for i in range(len(Hs.keys())):
+            channels = Hs[i]['channels']
+            for chan in channels:
+                j, omega, w, idx = chan
+
+                legendre_A = [self.vv[0,idx,j] * legendre(j)(2 * s / self.duration - 1) for j in range(self.n_basis)]
+                legendre_B = [self.vv[1,idx,j] * legendre(j)(2 * s / self.duration - 1) for j in range(self.n_basis)]
+                A = sum(legendre_A)
+                B = sum(legendre_B)
+                N = torch.sqrt(torch.square(A)+torch.square(B))
+                Ds = omega * (2 * sgm(N) - 1)/N *(np.cos(w * s) * A + np.sin(w * s) * B)
+                Ds.backward()
 
         dDdv = self.vv.grad.detach().numpy()
 
-        H = [_H[0]]
-        for i in range(1, len(_H)):
-            H.append([_H[i], self.full_pulse(self.vv.detach().numpy(), i=i-1)])
+        H = [H0]
+        for i in range(len(Hs.keys())):
+            ham = Hs[i]['H']
+            H.append([
+                ham, self.full_pulse(self.vv.detach().numpy(), Hs[i]['channels'])])
 
-        for i in range(1, len(_H)):
+        for i in range(1, len(H)):
             t0s = np.linspace(0, s, 10)
-            # print(self.duration)
             result = qp.mesolve(H, initial_state, t0s)
-            # print("H", H)
-            # print(initial_state)
-            # print("------------------------------")
             phi = result.states[-1]
             
             r = 1 / 2
             d = initial_state.shape[0]
-            gate_p = (qp.qeye(d) + r * 1.j * _H[i]) / np.sqrt(1. + r**2)
-            gate_m = (qp.qeye(d) - r * 1.j * _H[i]) / np.sqrt(1. + r**2)
+            gate_p = (qp.qeye(d) + r * 1.j * H[i][0]) / np.sqrt(1. + r**2)
+            gate_m = (qp.qeye(d) - r * 1.j * H[i][0]) / np.sqrt(1. + r**2)
                 
             ts1 = np.linspace(s, self.duration, 10)
             result = qp.mesolve(H, gate_p * phi, ts1)
-            # print("_H[i]", _H[i])
-            # print("phi", phi)
-            # print("gate_p * phi", gate_p * phi)
-            # print("gate_p ", gate_p )
-            # print("======================")
             ket_p = result.states[-1]
             ps_p = M.matrix_element(ket_p, ket_p)
             if self.is_noisy:
                 ps_p += np.random.normal(scale=np.abs(ps_p.real) / 5)
                 
             result = qp.mesolve(H, gate_m * phi, ts1)
-            # print("gate_m * phi", gate_m * phi)
-            # print("gate_m ", gate_m )
             ket_m = result.states[-1]
             ps_m = M.matrix_element(ket_m, ket_m)
 
-            # print("ket_p, ket_m", ket_p, ket_m)
-            # exit()
             if self.is_noisy:
                 ps_m += np.random.normal(scale=np.abs(ps_m.real) / 5)
 
             # print("--", ps_m, ps_p)
             ps = ((1 + r**2) / 2 / r * (ps_m - ps_p)).real
-            dDdv[:,i-1,:] = ps * dDdv[:,i-1,:]
-        
+
+            channels = Hs[i-1]['channels']
+            for chan in channels:
+                j, omega, w, idx = chan
+                dDdv[:,idx,:] = ps * dDdv[:,idx,:]
         return dDdv
         
 
 
-    def compute_energy_grad_MC(self, _H, M, initial_state, num_sample):
+    def compute_energy_grad_MC(self, H0, Hs, M, initial_state, num_sample):
         # (M, H, initial_state, num_sample):
         """Compute the gradient of engergy function <psi(T)|M|psi(T)>, T = duration
         Args:
@@ -232,14 +229,18 @@ class QubitControl(object):
 
         grad = np.zeros(self.vv.shape)
         for s in sample_time: 
-            grad += self.get_integrand(_H, M, initial_state, s)
+            grad += self.get_integrand(H0, Hs, M, initial_state, s)
         
         return torch.from_numpy(self.duration * grad / num_sample)
     
-    def compute_energy(self, _H, M, initial_state):    
-        H = [_H[0]]
-        for i in range(1, len(_H)):
-            H.append([_H[i], self.full_pulse(self.vv.detach().numpy(), i=i-1)])
+    def compute_energy(self, H0, Hs, M, initial_state):   
+        H = [H0]
+        for i in range(len(Hs.keys())):
+            ham = Hs[i]['H']
+            H.append([
+                ham, self.full_pulse(self.vv.detach().numpy(), Hs[i]['channels'])])
+                # _H[i], self.full_pulse(self.vv.detach().numpy(), i=i-1)])
+
         tarray = np.linspace(0, self.duration, 10)
         result = qp.mesolve(H, initial_state, tarray)
         psi_T = result.states[-1]  
@@ -248,7 +249,8 @@ class QubitControl(object):
     
 
     def IBM_H(self, n_qubit):
-        self.ibm_params = {'delta0': -2135551738.17207,
+        self.ibm_params = {
+        'delta0': -2135551738.17207,
           'delta1': -2156392705.7817025,
           'delta2': -2146430374.8958619,
           'delta3': -2143302106.510836,
@@ -262,6 +264,7 @@ class QubitControl(object):
           'jq4q5': 12857428.747059302,
           'jq5q6': 13142900.487599919,
           'omegad0': 955111374.7779446,
+          # 'omegad0': 955000000,
           'omegad1': 987150040.8532522,
           'omegad2': 985715793.6078007,
           'omegad3': 978645256.0180163,
@@ -269,6 +272,7 @@ class QubitControl(object):
           'omegad5': 1000100925.8075224,
           'omegad6': 976913592.7775077,
           'wq0': 32901013497.991684,
+          # 'wq0': 32901000000,
           'wq1': 31504959831.439907,
           'wq2': 32092824583.27148,
           'wq3': 32536784568.16119,
@@ -277,23 +281,36 @@ class QubitControl(object):
           'wq6': 33300775594.753788}
 
 
-          # raw_freq = 2 *  pi *
-          # h.get_qubit_lo_from_drift()
-          #   [5236376147.050786,
-          #    5014084426.228487,
-          #    5107774458.035009,
-          #    5178450242.236394,
-          #    5213406970.098254,
-          #    5063177674.197013,
-          #    5300001532.8865185]
+        # raw_freq = 2 *  pi *
+        self.get_qubit_lo_from_drift = np.array([
+            5236376147.050786,
+            5014084426.228487,
+            5107774458.035009,
+            5178450242.236394,
+            5213406970.098254,
+            5063177674.197013,
+            5300001532.8865185])
 
+        self.ws = self.get_qubit_lo_from_drift * 2 * np.pi * 1e-9 * self.dt
+
+
+        self.hams = {
+            0: [0, 1],
+            1: [1, 0, 3, 2],
+            2: [2, 1],
+            3: [3, 1, 5],
+            4: [4, 5],
+            5: [5, 3, 6, 4],
+            6: [6, 5]
+        }
 
         H0 = self.multi_kron(*[self.O for j in range(n_qubit)])
         I = self.multi_kron(*[self.I for j in range(n_qubit)])
 
         for i in range(n_qubit):
             zi = self.multi_kron(*[self.I if j not in [i] else self.Z for j in range(n_qubit)])
-            H0 += 1 / 2. * (I - zi) * self.eps
+            wq = 'wq{}'.format(i)
+            H0 += 1 / 2. * (I - zi) * self.ibm_params[wq] * self.dt * 1e-9
 
         sm = np.array([[0, 1], 
                     [0, 0]])
@@ -307,20 +324,30 @@ class QubitControl(object):
             sps.append(p)
             sms.append(m)
 
-        if n_qubit > 1:
-            Js = [[j, (j+1)%n_qubit] for j in range(n_qubit)]
-            # Js = [[0, 1], [1, 2], [1, 3], [3, 5], [4, 5], [5, 6]]
-            for j in Js:
-                H0 += (np.matmul(sps[j[0]], sms[j[1]]) + np.matmul(sms[j[0]], sps[j[1]])) * \
-                (self.ibm_params['jq0q1'] * 1e-9 * self.dt)
+        Js = [[0, 1], [1, 2], [1, 3], [3, 5], [4, 5], [5, 6]]
+        for j in Js:
+            if j[0] >= n_qubit or j[1] >= n_qubit:
+                continue
+            jqq =  'jq{}q{}'.format(j[0], j[1])
+            H0 += (np.matmul(sps[j[0]], sms[j[1]]) + np.matmul(sms[j[0]], sps[j[1]])) * \
+            (self.ibm_params[jqq] * 1e-9 * self.dt)
 
-        Hs = []
+        Hs = {}
+        self.n_funcs = 0
+        # j, omega, ws, idx
         for i in range(n_qubit):
-            Hs.append(self.multi_kron(*[self.I if j not in [i] else self.X for j in range(n_qubit)]))
-            # H = self.multi_kron(*[self.I if j not in [i, (i + 1) % n_qubit] else self.Z for j in range(n_qubit)]) 
-            # Hs.append(H)
-
-        return H0, Hs
+            H = self.multi_kron(*[self.I if j not in [i] else self.X for j in range(n_qubit)])
+            Hs[i] = {}
+            Hs[i]['H'] = qp.Qobj(H)
+            Hs[i]['channels'] = []
+            for j in self.hams[i]:
+                if j > i:
+                    continue
+                omega = 'omegad{}'.format(i)
+                Hs[i]['channels'].append([j, self.ibm_params[omega] * 1e-9 * self.dt, self.ws[j], self.n_funcs])
+                self.n_funcs += 1
+        print("self.n_funcs", self.n_funcs)
+        return qp.Qobj(H0), Hs
 
 
 
@@ -372,7 +399,7 @@ class QubitControl(object):
             loss_l2.backward()
             grad_vv = self.compute_energy_grad_MC(M, initial_state, num_sample)
             self.vv.grad = grad_vv
-            optimizer.step()
+            # optimizer.step()
 
             print("epoch: {:04d}, loss: {:.4f}, loss_energy: {:.4f}".format(
                 epoch, 
@@ -385,7 +412,7 @@ class QubitControl(object):
             
         return self.vv
             
-    def train_fidelity(self, vv0, H, initial_states, target_states, num_sample):
+    def train_fidelity(self, vv0, H0, Hs, initial_states, target_states, num_sample):
         """Train the sepctral coefficients to minimize energy minimization.
         Args:
             M: A Hermitian matrix.
@@ -396,7 +423,6 @@ class QubitControl(object):
         """
         
         self.vv = torch.tensor(vv0, requires_grad=True)
-        
         w_l2 = 0
         lr = self.lr
         optimizer = torch.optim.Adam([self.vv], lr=lr)
@@ -407,7 +433,6 @@ class QubitControl(object):
         I = qp.Qobj(I)
         initials = [qp.Qobj(v) for v in initial_states]
         targets = [qp.Qobj(v) for v in target_states]
-        H = [qp.Qobj(h) for h in H]
 
         for epoch in range(1, self.n_epoch + 1):
             if epoch % 20 == 0:
@@ -418,21 +443,9 @@ class QubitControl(object):
                 psi0 = initials[i]
                 psi1 = targets[i]
                 M = I - psi1 * psi1.dag() 
-                loss_fidelity = self.compute_energy(H, M, psi0)
-                # result = qp.mesolve(H, psi0, ts)
-                # final_state = result.states[-1]
-
-                # inner_product_norm = M.matrix_element(final_state, final_state)
-                # if self.is_noisy:
-                #     inner_product_norm += np.random.normal(
-                #         scale=np.abs(inner_product_norm.real) / 5)
-                # loss_fidelity = 1 - inner_product_norm
-                # loss_l2 = ((self.spectral_coeff**2).mean(0) * torch.tensor(
-                #     [i**2 for i in range(self.n_basis)])).mean() * w_l2
-                # loss = loss_fidelity + loss_l2
+                loss_fidelity = self.compute_energy(H0, Hs, M, psi0)
                 optimizer.zero_grad()
-                # loss_l2.backward()
-                grad_vv = self.compute_energy_grad_MC(H, M, psi0, num_sample) 
+                grad_vv = self.compute_energy_grad_MC(H0, Hs, M, psi0, num_sample) 
 
                 self.vv.grad = grad_vv
                 optimizer.step()
@@ -454,10 +467,9 @@ class QubitControl(object):
         self.n_qubit = n_qubit
 
         H0, Hs = self.IBM_H(n_qubit)
-        H = [H0] + Hs
-        self.n_Hs = len(Hs) 
-        vv0 =  np.random.rand(2 * self.n_basis * self.n_Hs)
-        vv0 = np.reshape(vv0, [2, self.n_Hs ,self.n_basis])
+        self.n_Hs = len(Hs.keys()) 
+        vv0 =  np.random.rand(2 * self.n_basis * self.n_funcs)
+        vv0 = np.reshape(vv0, [2, self.n_funcs ,self.n_basis])
 
         g = np.array([1,0])
         e = np.array([0,1])
@@ -466,30 +478,29 @@ class QubitControl(object):
         initial_states = [np.kron(g, g), np.kron(g, e), np.kron(e, g)]
         target_states = [np.kron(g, g), np.kron(g, e), np.kron(e, e)]
 
-        self.train_fidelity(vv0, H, initial_states, target_states, num_sample)
+        self.train_fidelity(vv0, H0, Hs, initial_states, target_states, num_sample)
         
     def demo_X(self, num_sample, method):
         n_qubit = 1
         self.n_qubit = n_qubit
         
         H0, Hs = self.IBM_H(n_qubit)
-        H = [H0] + Hs
+        # H = [H0] + Hs
 
-        print(H)
+        # print(H)
 
-        self.n_Hs = len(Hs) 
-        vv0 =  np.random.rand(2 * self.n_basis * self.n_Hs)
-        vv0 = np.reshape(vv0, [2, self.n_Hs ,self.n_basis])
+        self.n_Hs = len(Hs.keys()) 
+        vv0 =  np.random.rand(2 * self.n_basis * self.n_funcs)
+        print(self.n_funcs)
+        vv0 = np.reshape(vv0, [2, self.n_funcs ,self.n_basis])
         # vv0 = np.random.normal(0, 1e-3, [self.n_Hs ,self.n_basis]) 
 
         g = np.array([1,0])
         e = np.array([0,1])
-        # pres = ['00', '01', '10', '++']
-        # posts = ['00', '01', '11', '++']
-        initial_states = [g, 1/np.sqrt(2)*e + 1/np.sqrt(2)*g]
-        target_states = [e, 1/np.sqrt(2)*e + 1/np.sqrt(2)*g]
+        initial_states = [1/np.sqrt(2)*e + 1/np.sqrt(2)*g, g]
+        target_states = [1/np.sqrt(2)*e + 1/np.sqrt(2)*g, e]
 
-        self.train_fidelity(vv0, H, initial_states, target_states, num_sample)
+        self.train_fidelity(vv0, H0, Hs, initial_states, target_states, num_sample)
         
     
     def model_qubit(self, vv0, num_sample, method):
@@ -545,13 +556,13 @@ class QubitControl(object):
 
 if __name__ == '__main__':
 
-    # np.random.seed(0)
-    model = QubitControl(basis='Legendre', n_basis=5 , dt=0.22, duration=96, n_epoch=50, lr = 1e-2)
+    np.random.seed(0)
+    model = QubitControl(basis='Legendre', n_basis=5 , dt=0.22, duration=96, n_epoch=500, lr = 1e-2)
     num_sample = 5
     # vv0 = np.random.rand(model.n_basis)
     # num_sample = 1
     # g = model.model_qubit(vv0, num_sample, 'plain')
     # loss0 = model.losses_energy
-    # model.demo_CNOT(num_sample, 'plain')
-    # model.demo_X(num_sample, 'plain')
     model.demo_CNOT(num_sample, 'plain')
+    # model.demo_X(num_sample, 'plain')
+    # model.demo_CNOT(num_sample, 'plain')
